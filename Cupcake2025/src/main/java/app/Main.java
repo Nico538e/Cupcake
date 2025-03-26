7688eb7415484fc614da4e1fbb711ad0184e061b
Nico538e <nicolai-strand@hotmail.com>
2025-03-26 10:44:21 +0100

    mere javalin

--- Cupcake2025/src/main/java/app/Main.java
+++ Cupcake2025/src/main/java/app/Main.java
@@ -1,7 +1,21 @@
 package app;
 
+import io.javalin.Javalin;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+        public static void main(String[] args)
+        {
+            // Initializing Javalin and Jetty webserver
+
+            Javalin app = Javalin.create(config -> {
+                config.staticFiles.add("/public");
+                config.jetty.modifyServletContextHandler(handler ->  handler.setSessionHandler(SessionConfig.sessionConfig()));
+                config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+            }).start(7070);
+
+            // Routing
+
+            app.get("/", ctx ->  ctx.render("index.html"));
+        }
     }
 }
