b563a3124e45fb5e7a2cbce65ae9a1050fbfacb5
Nico538e <nicolai-strand@hotmail.com>
2025-03-26 12:36:35 +0100

    DB exception

--- Cupcake2025/src/main/java/app/exceptions/DatabaseException.java
+++ Cupcake2025/src/main/java/app/exceptions/DatabaseException.java
@@ -1,4 +1,21 @@
 package app.exceptions;
 
-public class DatabaseException {
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DatabaseException extends Exception
+{
+    private static final Logger LOGGER = Logger.getLogger(DatabaseException.class.getName());
+
+    public DatabaseException(String userMessage)
+    {
+        super(userMessage);
+        LOGGER.log(Level.WARNING, userMessage);
+    }
+
+    public DatabaseException(String userMessage, String systemMessage) {
+        super(userMessage);
+        LOGGER.log(Level.WARNING, userMessage);
+        LOGGER.log(Level.WARNING, "errorMessage: " + systemMessage);
+    }
 }
