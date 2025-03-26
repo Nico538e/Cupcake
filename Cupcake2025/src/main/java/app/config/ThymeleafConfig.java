7b096eab270ada081afbcf9b6630e1da7cbb8288
Peter399c1 <cph-pm260@cphbusiness.dk>
2025-03-26 09:58:36 +0100

    Thymeleaf sessionHandler and update pom.xml file

--- Cupcake2025/src/main/java/app/config/ThymeleafConfig.java
+++ Cupcake2025/src/main/java/app/config/ThymeleafConfig.java
@@ -1,4 +1,20 @@
 package app.config;
 
+import jakarta.servlet.SessionTrackingMode;
+import org.eclipse.jetty.server.session.SessionHandler;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import java.util.EnumSet;
+
 public class ThymeleafConfig {
+    public static TemplateEngine templateEngine()
+    {
+        TemplateEngine templateEngine = new TemplateEngine();
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/"); // assuming templates are in resources/templates/
+        templateResolver.setSuffix(".html");
+        templateEngine.setTemplateResolver(templateResolver);
+        return templateEngine;
+    }
 }
