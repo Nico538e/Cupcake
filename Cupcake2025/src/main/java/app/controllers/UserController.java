504ce510fdb66d2bf8d8787d54e5a58347e68904
Nico538e <nicolai-strand@hotmail.com>
2025-04-01 19:37:02 +0200

    tilføjet cupcake klasse og metode til controller, ændringer til index, inspiration og shoppingcart siderne

--- Cupcake2025/src/main/java/app/controllers/UserController.java
+++ Cupcake2025/src/main/java/app/controllers/UserController.java
@@ -3,6 +3,7 @@ package app.controllers;
 
 
 import app.entities.Bottom;
+import app.entities.Cupcake;
 import app.entities.Topping;
 import app.entities.User;
 import app.exceptions.DatabaseException;
@@ -12,6 +13,7 @@ import io.javalin.Javalin;
 import io.javalin.http.Context;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserController {
@@ -24,8 +26,9 @@ public class UserController {
         app.get("signUp", ctx -> ctx.render("signUp.html"));// get-methoden bruges til at komme fra forside.hmtl til opretBruger.
         app.post("/signUp", ctx -> createuser(ctx, connectionPool));//post-methoden bruges til at lave en ny bruger og gå tilbage til forside.html
         app.get("/inspiration", ctx -> ctx.render("inspiration.html"));
-        app.get("/shoppingcart", ctx -> ctx.render("shoppingcart.html"));
+        app.get("/shoppingCart", ctx -> ctx.render("shoppingcart.html"));
         app.post("/showCupcakes", ctx -> UserController.getCupcakeOptions(ctx, connectionPool));
+        app.post("/shoppingCart", ctx -> addToShoppingCart(ctx,connectionPool));
     }
 
 
@@ -81,7 +84,7 @@ public class UserController {
                 //opretter bruger i db
                 CupcakeMapper.createuser(email, password1, connectionPool);
                 //Sender succes besked
-                ctx.attribute("message","Du er nu blevet opretter med email:" + email + " Du kan nu logge ind.");
+                ctx.attribute("message","Du er nu blevet oprettet med email: \n" + email + " Du kan nu logge ind.");
                 //sender brugeren til login.html
                 ctx.render("/signUp");
 
@@ -112,6 +115,31 @@ public class UserController {
             ctx.attribute("message", "Fejl ved hentning af cupcake data");
             ctx.render("error.html");
         }
+    }
+
+    public static void addToShoppingCart(Context ctx, ConnectionPool connectionPool) throws DatabaseException {
+        int bottomId = Integer.parseInt(ctx.formParam("bottom"));
+        int toppingId = Integer.parseInt(ctx.formParam("topping"));
+        int quantity = Integer.parseInt(ctx.formParam("quantity"));
+
+        Bottom bottom = CupcakeMapper.getOneBottomById(connectionPool, bottomId);
+        Topping topping = CupcakeMapper.getOneToppingById(connectionPool, toppingId);
+
+
+        Cupcake cupcake = new Cupcake(bottom, topping, quantity);
+
+        List<Cupcake> shoppingCart = ctx.sessionAttribute("shoppingCart");
+
+        if(shoppingCart == null){
+            shoppingCart = new ArrayList<>();
+        }
+
+        shoppingCart.add(cupcake);
+
+        ctx.sessionAttribute("shoppingCart", shoppingCart);
+
+        ctx.redirect("/shoppingCart");
+
     }
 
 
---
+++ Cupcake2025/src/main/java/app/entities/Cupcake.java
@@ -0,0 +1,37 @@
+package app.entities;
+
+public class Cupcake {
+    private Bottom bottom;
+    private Topping topping;
+    private int quantity;
+
+    public Cupcake(Bottom bottom, Topping topping, int quantity) {
+        this.bottom = bottom;
+        this.topping = topping;
+        this.quantity = quantity;
+    }
+
+    public Bottom getBottom() {
+        return bottom;
+    }
+
+    public void setBottom(Bottom bottom) {
+        this.bottom = bottom;
+    }
+
+    public Topping getTopping() {
+        return topping;
+    }
+
+    public void setTopping(Topping topping) {
+        this.topping = topping;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
